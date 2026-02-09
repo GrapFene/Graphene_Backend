5b5e3f0c6e49a1753616c2e3930b984b005449b2
adofm <karthik.maniam008@gmail.com>
2026-02-09 23:28:12 +0530

    feat: add services index

---
+++ src/services/index.ts
@@ -0,0 +1,3 @@
+export { getSupabase } from './supabase.js';
+export { verifySignature, generateNonce } from './crypto.js';
+export { createChallenge, verifyLogin, registerUser, initiateMnemonicLogin, verifyMnemonicLogin } from './auth.js';
