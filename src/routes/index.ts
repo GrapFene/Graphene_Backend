8d7b081370a0612b97b6f2399434387806ff7e3d
adofm <karthik.maniam008@gmail.com>
2026-02-11 15:24:48 +0530

    feat: register new modular routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,9 @@
-export { default as authRouter } from './auth.js';
+import authRouter from './auth.js';
+import { postRouter } from './post.js';
+import { subscriptionRouter } from './subscription.js';
+import { communityRouter } from './community.js';
+import { voteRouter } from './vote.js';
+import { proposalRouter } from './proposal.js';
+import { blockRouter } from './block.js';
+
+export { authRouter, postRouter, subscriptionRouter, communityRouter, voteRouter, proposalRouter, blockRouter };
