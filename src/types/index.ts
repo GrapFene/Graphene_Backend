c58995d70b7e650a69ffb9c7350c5eb0e3ae3f48
Karthik Vishal <karthikvishal1506@gmail.com>
2026-02-14 19:40:37 +0530

    New RegisterRequest function created for metadata

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,10 @@
-export * from './database.js';
-export * from './api.js';
-export * from './moderation.js';
-export * from './moderation-api.js';
-export * from './recovery.js';
-export * from './retry.js';
+export interface RegisterRequest {
+    username: string;
+    salt: string;
+    public_key: string;
+    mnemonic_hashes: string[];
+    // New: Initial Profile Metadata
+    profile_content?: any;
+    profile_signed_hash?: string;
+    profile_nonce?: string;
+}
